4f50f82fa9a41dd57b38ceb3bdc20244b4561a91
Maros Kukan <maros.kukan@me.com>
2021-03-22 16:42:13 +0100

    Copy to /usr/bin

--- installation/containerd/containerd.from.github.sh
+++ installation/containerd/containerd.from.github.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Download and extract the containerd binaries
 VERSION="1.4.4"
 FILE="containerd-${VERSION}-linux-amd64.tar.gz"
 wget "https://github.com/containerd/containerd/releases/download/v${VERSION}/${FILE}"
 tar xvf "${FILE}"
+
+sudo cp bin/* /usr/bin
