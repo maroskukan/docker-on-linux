85c32d77af2d67d3529d5cb6b489b2f0de6c1d2a
Maros Kukan <13705278+maroskukan@users.noreply.github.com>
2022-06-21 23:05:44 +0200

    Add repo workaround to CentOS-8 and multi provider support

--- installation/centos-8/provision.sh
+++ installation/centos-8/provision.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+# Workaround for CentOS 8 (EOL 2024-05-31)
+# Error: Failed to download metadata for repo 'appstream': Cannot prepare internal mirrorlist: No URLs in mirrorlist
+sudo sed -i 's/mirrorlist=/#mirrorlist=/g' \
+            /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' \
+            /etc/yum.repos.d/CentOS-*
+
 # Docker CE for Linux installation script
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo curl -sSL https://get.docker.com | sh
 
 # Start the service and enable on boot
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Add vagrant user to docker group
-usermod -aG docker vagrant
+sudo usermod -aG docker vagrant
+
+# Validate installation by running a sample image
+sudo docker run --rm hello-world
